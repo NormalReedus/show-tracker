5122736b0882140a01678057a598ae6c82fea637
NormalReedus <magnus.borregaard@gmail.com>
2021-05-08 15:21:07 +0200

    merge {N} and backend

---
+++ app/app.js
@@ -0,0 +1,10 @@
+import VueDevtools from "nativescript-vue-devtools";
+import Vue from "nativescript-vue";
+
+Vue.use(VueDevtools);
+
+import Home from "./components/Home";
+
+new Vue({
+  render: h => h("frame", [h(Home)])
+}).$start();
